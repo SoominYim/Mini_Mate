ea746388eccf993691873b889db8577f633d2ad6
SoominYim <soo15soo@kakao.com>
2024-01-23 14:02:28 +0900

    footer, header Update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
+    <div id="header">
+      <mini-header></mini-header>
+    </div>
+    <div id="content">
+      <v-main>
+        <router-view />
+      </v-main>
+    </div>
+    <div id="footer">
+      <mini-footer></mini-footer>
+    </div>
   </v-app>
 </template>
 
 <script>
-
+import MiniFooter from './components/common/MiniFooter.vue';
+import MiniHeader from './components/common/MiniHeader.vue';
 export default {
   name: 'App',
-
+  components: {
+    MiniFooter,
+    MiniHeader,
+  }
+  ,
   data: () => ({
     //
   }),
 }
 </script>
+<style lang="scss">
+@import "@/css/reset.css";
+
+
+#footer {
+  position: fixed;
+  width: 100%;
+  height: 150px;
+  box-sizing: border-box;
+  background: #333;
+  padding: 30px;
+  bottom: 0;
+  left: 0;
+}
+</style>
---
+++ src/components/common/MiniFooter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <a href="https://github.com/SoominYim" title="git_hub" class="sns" target="blank">
+      <i class="fa-brands fa-github"></i>
+    </a>
+    <a href="https://www.instagram.com/_soo_min_/" title="instagram" class="sns" target="blank">
+      <i class="fa-brands fa-instagram"></i>
+    </a>
+    <a href="https://www.facebook.com/profile.php?id=100002681110982" title="facebook" class="sns" target="blank"> <i
+        class="fa-brands fa-facebook"></i>
+    </a>
+    <a href="https://s-o-o-min.tistory.com/" title="tistory" class="sns" target="blank"> <i class="fa-solid fa-t"></i></a>
+    <span>© {{ new Date().getFullYear() }}. S-o-o-Min . All rights reserved.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogFooter",
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  height: 100px;
+  text-align: center;
+
+  .sns {
+    color: #fff;
+    font-size: 40px;
+    padding: 10px;
+
+    &:hover {
+      color: #888;
+    }
+
+    transition: all 0.5s ease;
+  }
+
+  span {
+    font-size: 14px;
+    display: block;
+    margin-top: 20px;
+    color: #fff;
+  }
+}
+</style>
