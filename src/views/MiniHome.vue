b328fe1cf005a0da329427f0e1a3a7a744ca496a
SoominYim <soo15soo@kakao.com>
2024-01-25 19:51:19 +0900

    .env del

--- src/views/MiniHome.vue
+++ src/views/MiniHome.vue
@@ -8,13 +8,8 @@
         </div>
         <div class="weather-box">
           <div class="temp">{{ weather.main.temp.toFixed(1) }}°</div>
-          <v-img
-            :width="100"
-            :aspect-ratio="1"
-            class="mx-auto"
-            cover
-            :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"
-          ></v-img>
+          <v-img :width="100" :aspect-ratio="1" class="mx-auto" cover
+            :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"></v-img>
           <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
@@ -35,10 +30,11 @@ export default {
   computed: {
     ...mapState("weatherStore", ["url_base", "months", "days"]),
   },
-  setup() {},
-  created() {},
+  setup() { },
+  created() { },
   mounted() {
-    let fetchUrl = `${this.url_base}weather?q=seoul&units=metric&APPID=${process.env.VUE_APP_WEATHER_API_KEY}`;
+    // let fetchUrl = `${this.url_base}weather?q=seoul&units=metric&APPID=${process.env.VUE_APP_WEATHER_API_KEY}`;
+    let fetchUrl = `${this.url_base}weather?q=seoul&units=metric&APPID=f6876c452d89444b12d22a31a89942ad`;
     fetch(fetchUrl)
       .then((res) => {
         return res.json();
@@ -47,7 +43,7 @@ export default {
         return this.setResult(results);
       });
   },
-  unmounted() {},
+  unmounted() { },
   methods: {
     setResult: function (results) {
       this.weather = results;
@@ -71,15 +67,18 @@ export default {
   background-position: bottom;
   transition: 0.4s;
   font-family: "montserrat", sans-serif;
+
   main {
     height: 100%;
     padding: 25px;
     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
   }
+
   .search-box {
     width: 100%;
     margin-bottom: 30px;
   }
+
   .search-box .search-bar {
     display: block;
     width: 100%;
@@ -97,11 +96,13 @@ export default {
     transition: 0.4s;
     border-radius: 0px 16px 0px 16px;
   }
+
   .search-box .search-bar:focus {
     box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
     background-color: rgba(255, 255, 255, 0.9);
     border-radius: 16px 0px 16px 0px;
   }
+
   .location-box .location {
     color: #fff;
     font-size: 32px;
@@ -109,6 +110,7 @@ export default {
     text-align: center;
     text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
   }
+
   .location-box .date {
     color: #fff;
     font-size: 20px;
@@ -116,9 +118,11 @@ export default {
     font-style: italic;
     text-align: center;
   }
+
   .weather-box {
     text-align: center;
   }
+
   .weather-box .temp {
     display: inline-block;
     padding: 10px 25px;
@@ -131,6 +135,7 @@ export default {
     margin: 30px 0px;
     box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
   }
+
   .weather-box .weather {
     color: #fff;
     font-size: 48px;
