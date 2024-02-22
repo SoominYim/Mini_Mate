a4e113c7ed1f277cb39a4f6ce6987dfdb4d8b9ff
SoominYim <soo15soo@kakao.com>
2024-02-22 14:05:58 +0900

    Card Text Color

--- src/views/MiniHome.vue
+++ src/views/MiniHome.vue
@@ -1,114 +1,125 @@
 <template>
-  <div id="main">
-    <mini-star v-show="new Date().getHours() >= 20 || new Date().getHours() < 6"></mini-star>
-    <main :style="backgroundStyle">
-      <mini-snow v-show="isSnow"></mini-snow>
-      <mini-rain v-show="isRain"></mini-rain>
+  <mini-star v-show="new Date().getHours() >= 20 || new Date().getHours() < 6"></mini-star>
+  <main :style="backgroundStyle">
+    <mini-snow v-show="isSnow"></mini-snow>
+    <mini-rain v-show="isRain"></mini-rain>
 
-      <div class="weather-wrap" v-if="weatherData.main !== undefined && weatherDaily.list !== undefined">
-        <div class="location-box">
-          <div class="location">서울</div>
-          <div class="date">{{ getDate }}</div>
-        </div>
-        <div class="weather-box">
-          <div class="temp">
-            <div>{{ filteredWeatherList[0].main.temp.toFixed(1) }}°</div>
-            <div class="min_max">
-              <span class="min">
-                {{
-                  weatherDaily.list
-                    .filter((v) => new Date().getDate() === new Date(v.dt_txt).getDate())
-                    .map((v) => v.main.temp)
-                    .reduce((min, temp) => Math.min(min, temp), Infinity)
-                    .toFixed(1)
-                }}°</span
-              >/
-              <span class="max">
-                {{
-                  weatherDaily.list
-                    .filter((v) => new Date().getDate() === new Date(v.dt_txt).getDate())
-                    .map((v) => v.main.temp)
-                    .reduce((max, temp) => Math.max(max, temp), -Infinity)
-                    .toFixed(1)
-                }}°</span
-              >
-            </div>
-          </div>
-          <!-- <div>{{ weatherData.main }}</div> -->
-          <v-img
-            :width="100"
-            :aspect-ratio="1"
-            class="mx-auto"
-            cover
-            :src="`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`"
-          ></v-img>
-          <div class="weather">{{ weatherData.weather[0].main }}</div>
-        </div>
-
-        <v-sheet class="mx-auto mt-8" elevation="3" max-width="800" theme="false" rounded>
-          <v-slide-group v-model="model" class="pa-4" selected-class="selected" center-active>
-            <v-slide-group-item
-              v-for="item in filteredWeatherList"
-              :key="item.dt"
-              v-slot="{ isSelected, toggle, selectedClass }"
-            >
-              <v-card
-                color="rgba(255, 255, 255, 0.25)"
-                :class="['ma-4', selectedClass]"
-                height="110"
-                width="70"
-                @click="toggle"
-              >
-                <div class="d-flex fill-height align-center flex-column justify-center pb-3">
-                  <v-scale-transition>
-                    <v-img
-                      :width="60"
-                      :aspect-ratio="1"
-                      class="mx-auto"
-                      cover
-                      :src="`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`"
-                    ></v-img>
-                    <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"> </v-icon>
-                  </v-scale-transition>
-                  <div class="text-center date-text">
-                    {{ item.main.temp.toFixed(1) }}° <br />
-                    {{ formatDate(item.dt_txt) }}
-                  </div>
-                </div>
-              </v-card>
-            </v-slide-group-item>
-          </v-slide-group>
-
-          <v-expand-transition>
-            <v-sheet v-if="model != null" height="130">
-              <div class="d-flex fill-height align-center justify-center">
-                <h3 class="text-h6">
-                  강수 확률: {{ ~~(filteredWeatherList[model].pop * 100) }}%
-                  <br />
-                  강수량:
-                  {{
-                    filteredWeatherList[model].snow
-                      ? filteredWeatherList[model].snow["3h"]
-                        ? filteredWeatherList[model].snow["3h"] + "mm"
-                        : "데이터 없음"
-                      : filteredWeatherList[model].rain
-                      ? filteredWeatherList[model].rain["3h"]
-                        ? filteredWeatherList[model].rain["3h"] + "mm"
-                        : "데이터 없음"
-                      : "데이터 없음"
-                  }}
-                  <br />
-                  바람 속도: {{ filteredWeatherList[model].wind.speed }}m/s
-                  <br />
-                  습도: {{ filteredWeatherList[model].main.humidity }}%
-                </h3>
-              </div>
-            </v-sheet>
-          </v-expand-transition>
-        </v-sheet>
+    <div class="weather-wrap" v-if="weatherData.main !== undefined && weatherDaily.list !== undefined">
+      <div class="location-box">
+        <div class="location">서울</div>
+        <div class="date">{{ getDate }}</div>
       </div>
-    </main>
-  </div>
+      <div class="weather-box">
+        <div class="temp">
+          <div>{{ filteredWeatherList[0].main.temp.toFixed(1) }}°</div>
+          <div class="min_max">
+            <span class="min">
+              {{
+                weatherDaily.list
+                  .filter((v) => new Date().getDate() === new Date(v.dt_txt).getDate())
+                  .map((v) => v.main.temp)
+                  .reduce((min, temp) => Math.min(min, temp), Infinity)
+                  .toFixed(1)
+              }}°</span
+            >/
+            <span class="max">
+              {{
+                weatherDaily.list
+                  .filter((v) => new Date().getDate() === new Date(v.dt_txt).getDate())
+                  .map((v) => v.main.temp)
+                  .reduce((max, temp) => Math.max(max, temp), -Infinity)
+                  .toFixed(1)
+              }}°</span
+            >
+          </div>
+        </div>
+        <!-- <div>{{ weatherData.main }}</div> -->
+        <v-img
+          :width="100"
+          :aspect-ratio="1"
+          class="mx-auto"
+          cover
+          :src="`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`"
+        ></v-img>
+        <div class="weather">{{ weatherData.weather[0].main }}</div>
+      </div>
+
+      <v-sheet class="mx-auto mt-8" elevation="3" max-width="800" theme="false" rounded>
+        <v-slide-group v-model="model" class="pa-4" selected-class="selected" center-active>
+          <v-slide-group-item
+            v-for="(item, i) in filteredWeatherList"
+            :key="i"
+            v-slot="{ isSelected, toggle, selectedClass }"
+          >
+            <v-card
+              color="rgba(255, 255, 255, 0.25)"
+              :class="['ma-4', selectedClass]"
+              height="130"
+              width="70"
+              @click="toggle"
+            >
+              <div class="isDay pt-2">
+                <div v-if="new Date(item.dt_txt).getDate() === new Date().getDate()" class="today">오늘</div>
+                <div v-else-if="new Date(item.dt_txt).getDate() === new Date().getDate() + 1" class="tomorrow">
+                  내일
+                </div>
+                <div v-else-if="new Date(item.dt_txt).getDate() === new Date().getDate() + 2" class="afterTomorrow">
+                  내일모레
+                </div>
+                <div v-else-if="new Date(item.dt_txt).getDate() === new Date().getDate() + 3" class="postAfterTomorrow">
+                  글피
+                </div>
+                <div class="postAfterTomorrow" v-else>먼훗날</div>
+              </div>
+              <div class="d-flex fill-height align-center flex-column justify-center pb-6">
+                <v-scale-transition>
+                  <v-img
+                    :width="60"
+                    :aspect-ratio="1"
+                    class="mt-10"
+                    cover
+                    :src="`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`"
+                  ></v-img>
+                  <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"> </v-icon>
+                </v-scale-transition>
+                <div class="text-center date-text" :class="isDay">
+                  {{ formatDate(item.dt_txt) }}<br />
+                  {{ item.main.temp.toFixed(1) }}°
+                </div>
+              </div>
+            </v-card>
+          </v-slide-group-item>
+        </v-slide-group>
+
+        <v-expand-transition>
+          <v-sheet v-if="model != null" height="130">
+            <div class="d-flex fill-height align-center justify-center">
+              <h3 class="text-h6">
+                강수 확률: {{ ~~(filteredWeatherList[model].pop * 100) }}%
+                <br />
+                강수량:
+                {{
+                  filteredWeatherList[model].snow
+                    ? filteredWeatherList[model].snow["3h"]
+                      ? filteredWeatherList[model].snow["3h"] + "mm"
+                      : "데이터 없음"
+                    : filteredWeatherList[model].rain
+                    ? filteredWeatherList[model].rain["3h"]
+                      ? filteredWeatherList[model].rain["3h"] + "mm"
+                      : "데이터 없음"
+                    : "데이터 없음"
+                }}
+                <br />
+                바람 속도: {{ filteredWeatherList[model].wind.speed }}m/s
+                <br />
+                습도: {{ filteredWeatherList[model].main.humidity }}%
+              </h3>
+            </div>
+          </v-sheet>
+        </v-expand-transition>
+      </v-sheet>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -206,123 +217,143 @@ export default {
     ...mapActions("weatherStore", ["fetchData", "fetchDataDaily"]),
     formatDate(t) {
       const d = new Date(t);
-      return `${d.getDate()}일\n${this.days[d.getDay()]}\n${d.getHours()}시`;
+      return `${d.getHours()}시\n${this.days[d.getDay()]}`;
     },
   },
 };
 </script>
 <style scoped>
-#main {
+main {
   height: 100%;
-  transition: 0.4s;
   font-family: "montserrat", sans-serif;
-  main {
-    height: 100%;
-    padding: 25px;
+  padding: 25px;
+  transition: 0.4s;
 
-    /* 06:00 ~ 09:00 */
-    /* background-image: linear-gradient(to bottom, rgba(220, 66, 37, 0.6), rgba(0, 47, 75, 0.8)); */
-    /* 09:00 ~ 12:00 */
-    /* background: linear-gradient(
+  /* 06:00 ~ 09:00 */
+  /* background-image: linear-gradient(to bottom, rgba(220, 66, 37, 0.6), rgba(0, 47, 75, 0.8)); */
+  /* 09:00 ~ 12:00 */
+  /* background: linear-gradient(
       rgba(56, 127, 251, 0.8) 0%,
       rgba(52, 196, 255, 0.8) 30%,
       rgba(255, 198, 208, 0.8) 75%,
       rgba(255, 206, 170, 0.8) 95%
       ); */
-    /* 12:00 ~ 17:00 */
-    /* if : Rain or snow */
-    /* background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75)); */
-    /* else*/
-    /* background: linear-gradient(to bottom, rgba(179, 140, 34, 0.75), rgba(236, 95, 24, 0.75)); */
-    /* 17:00 ~ 20:00 */
-    /* background-image: linear-gradient(to bottom, rgba(0, 47, 75, 0.8), rgba(220, 66, 37, 0.6)); */
-    /* 20:00 ~ 06:00 */
-    /* background-image: linear-gradient(315deg, #2d3436 30%, #000000 74%); */
-  }
-  .search-box {
-    width: 100%;
-    margin-bottom: 30px;
-  }
-  .search-box .search-bar {
-    display: block;
-    width: 100%;
-    padding: 15px;
+  /* 12:00 ~ 17:00 */
+  /* if : Rain or snow */
+  /* background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75)); */
+  /* else*/
+  /* background: linear-gradient(to bottom, rgba(179, 140, 34, 0.75), rgba(236, 95, 24, 0.75)); */
+  /* 17:00 ~ 20:00 */
+  /* background-image: linear-gradient(to bottom, rgba(0, 47, 75, 0.8), rgba(220, 66, 37, 0.6)); */
+  /* 20:00 ~ 06:00 */
+  /* background-image: linear-gradient(315deg, #2d3436 30%, #000000 74%); */
+}
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
 
-    color: #313131;
-    font-size: 20px;
-    appearance: none;
-    border: none;
-    outline: none;
-    background: none;
-    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-    background-color: rgba(255, 255, 255, 0.8);
-    border: rgba(0, 0, 0, 0);
-    transition: 0.4s;
-    border-radius: 0px 16px 0px 16px;
-  }
-  .search-box .search-bar:focus {
-    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-    background-color: rgba(255, 255, 255, 0.9);
-    border-radius: 16px 0px 16px 0px;
-  }
-  .location-box .location {
-    color: #fff;
-    font-size: 32px;
-    font-weight: 500;
-    text-align: center;
-    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-  }
-  .location-box .date {
-    color: #fff;
-    font-size: 20px;
-    font-weight: 300;
-    font-style: italic;
-    text-align: center;
-  }
-  .weather-box {
-    text-align: center;
-  }
-  .weather-box .temp {
-    display: inline-block;
-    padding: 10px 25px;
-    color: #fff;
-    font-size: 102px;
-    font-weight: 900;
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-    background-color: rgba(255, 255, 255, 0.25);
-    border-radius: 16px;
-    margin: 30px 0px;
-    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  }
-  .weather-box .weather {
-    color: #fff;
-    font-size: 48px;
-    font-weight: 700;
-    font-style: italic;
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  }
-  .min_max {
-    font-size: 50px;
-    text-shadow: 1px 2px rgba(0, 0, 0, 0.25);
-  }
-  .min {
-    color: #2178ef;
-  }
-  .max {
-    color: #cd3534;
-  }
-  .date-text {
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.8);
+  border: rgba(0, 0, 0, 0);
+  transition: 0.4s;
+  border-radius: 0px 16px 0px 16px;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.9);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.min_max {
+  font-size: 50px;
+  text-shadow: 1px 2px rgba(0, 0, 0, 0.25);
+}
+.min {
+  color: #2178ef;
+}
+.max {
+  color: #cd3534;
+}
+.date-text {
+  color: #444;
+  font-size: 15px;
+  font-weight: bold;
+  white-space: pre-line;
+}
+.v-sheet {
+  background: rgba(255, 255, 255, 0.25);
+}
+.selected {
+  --v-theme-overlay-multiplier: var(--v-theme-success-overlay-multiplier);
+  background-color: rgba(255, 255, 255, 0.5) !important;
+  color: rgba(255, 255, 255, 0.5) !important;
+}
+.isDay {
+  position: absolute;
+  width: 70px;
+  font-size: 18px;
+  font-weight: bold;
+  z-index: 10;
+  .today {
     color: #444;
-    font-size: 15px;
-    font-weight: bold;
   }
-  .v-sheet {
-    background: rgba(255, 255, 255, 0.25);
+  .tomorrow {
+    color: #7a59f1;
   }
-  .selected {
-    --v-theme-overlay-multiplier: var(--v-theme-success-overlay-multiplier);
-    background-color: rgba(255, 255, 255, 0.5) !important;
-    color: rgba(255, 255, 255, 0.5) !important;
+  .afterTomorrow {
+    color: #31889d;
   }
+  .postAfterTomorrow {
+    color: #975220;
+  }
+}
+.isDay * {
+  text-align: center;
 }
 </style>
